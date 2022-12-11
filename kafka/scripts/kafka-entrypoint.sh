4c11cf8cc9c243dd4ca44796d749be4b9f3a498e
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2022-12-11 15:21:39 +0100

    infra:add support for KAFKA_BOOTSTRAP_SERVERS variable
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- kafka/scripts/kafka-entrypoint.sh
+++ kafka/scripts/kafka-entrypoint.sh
@@ -12,7 +12,7 @@
 #
 [ "${KAFKA_BROKER_ID}" != "" ] || KAFKA_BROKER_ID="1"
 [ "${KAFKA_ZOOKEEPER_CONNECT}" != "" ] || KAFKA_ZOOKEEPER_CONNECT="zookeeper:2181"
-[ "${KAFKA_ADVERTISED_LISTENERS}" != "" ] || KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka:9092"
+[ "${KAFKA_ADVERTISED_LISTENERS}" != "" ] || KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://localhost:9092"
 [ "${ZOOKEEPER_CLIENT_PORT}" != "" ] || ZOOKEEPER_CLIENT_PORT="2191"
 [ "${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP}" != "" ] || KAFKA_LISTENER_SECURITY_PROTOCOL_MAP="PLAINTEXT:PLAINTEXT"
 [ "${KAFKA_INTER_BROKER_LISTENER_NAME}" != "" ] || KAFKA_INTER_BROKER_LISTENER_NAME="PLAINTEXT"
@@ -23,15 +23,15 @@ function create_topics {
     sleep 2
     for item in ${KAFKA_TOPICS}
     do
-        while ! "${KAFKA_HOME}/bin/kafka-topics.sh" --topic "${item}" --bootstrap-server localhost:9092 --describe; do
-            "${KAFKA_HOME}/bin/kafka-topics.sh" --create --bootstrap-server localhost:9092 --topic "${item}"
+        while ! "${KAFKA_HOME}/bin/kafka-topics.sh" --topic "${item}" --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" --describe; do
+            "${KAFKA_HOME}/bin/kafka-topics.sh" --create --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" --topic "${item}"
             sleep 1
         done
     done
-    "${KAFKA_HOME}/bin/kafka-topics.sh" --bootstrap-server localhost:9092 --list
+    "${KAFKA_HOME}/bin/kafka-topics.sh" --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" --list
     for item in ${KAFKA_TOPICS}
     do
-        "${KAFKA_HOME}/bin/kafka-topics.sh" --topic "${item}" --bootstrap-server localhost:9092 --describe
+        "${KAFKA_HOME}/bin/kafka-topics.sh" --topic "${item}" --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" --describe
     done
     exec 2>/dev/null 1>/dev/null
     while true; do sleep 5; done
