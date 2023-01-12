8d5a20078eaf6640593a751bf99e753956479ac6
Alejandro Visiedo <avisiedo@redhat.com>
2023-01-12 15:31:13 +0100

    move docker-compose.yaml to deployments/ , update mk/deps.mk and add rule compose-build
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/deps.mk
+++ mk/deps.mk
@@ -6,16 +6,28 @@
 # Requires 'mk/pulp.mk'
 ##
 
+COMPOSE_PROJECT_NAME ?= cs
+
 .PHONY: deps-up
 compose-up: $(GO_OUTPUT)/dbmigrate
 	$(DATABASE_COMPOSE_OPTIONS) \
 	$(KAFKA_COMPOSE_OPTIONS) \
-	$(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f compose/docker-compose.yml up --detach
+	$(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f deployments/docker-compose.yaml up --detach
 	$(MAKE) .db-health-wait
 	$(MAKE) db-migrate-up
 	@echo "Run 'make db-migrate-seed' to seed the database"
 
-.PHONY: deps-down
+.PHONY: compose-down
 compose-down:
 	$(DATABASE_COMPOSE_OPTIONS) \
-	$(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f compose/docker-compose.yml down --volumes
+	$(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f deployments/docker-compose.yaml down --volumes
+
+.PHONY: compose-clean
+compose-clean: compose-down
+	$(DOCKER) volume prune
+
+.PHONY: compose-build
+compose-build:
+	$(DATABASE_COMPOSE_OPTIONS) \
+	$(KAFKA_COMPOSE_OPTIONS) \
+	$(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f deployments/docker-compose.yaml build
