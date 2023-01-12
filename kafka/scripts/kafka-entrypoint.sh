04e607a3a8421fc17fe44b0e64042ed605c646ba
Alejandro Visiedo <avisiedo@redhat.com>
2023-01-12 15:30:09 +0100

    update configuration that worked locally
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- kafka/scripts/kafka-entrypoint.sh
+++ kafka/scripts/kafka-entrypoint.sh
@@ -10,13 +10,14 @@
 #   KAFKA_INTER_BROKER_LISTENER_NAME=PLAINTEXT
 #   KAFKA_OPTS='-javaagent:/usr/jolokia/agents/jolokia-jvm.jar=host=0.0.0.0'
 #
+[ "${ZOOKEEPER_HOST}" != "" ] || ZOOKEEPER_HOST="zookeeper"
 [ "${KAFKA_BROKER_ID}" != "" ] || KAFKA_BROKER_ID="1"
-[ "${KAFKA_ZOOKEEPER_CONNECT}" != "" ] || KAFKA_ZOOKEEPER_CONNECT="zookeeper:2181"
-[ "${KAFKA_ADVERTISED_LISTENERS}" != "" ] || KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka:9092"
+[ "${KAFKA_ZOOKEEPER_CONNECT}" != "" ] || KAFKA_ZOOKEEPER_CONNECT="${ZOOKEEPER_HOST}:2181"
+[ "${KAFKA_ADVERTISED_LISTENERS}" != "" ] || KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://localhost:9092"
 [ "${ZOOKEEPER_CLIENT_PORT}" != "" ] || ZOOKEEPER_CLIENT_PORT="2191"
 [ "${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP}" != "" ] || KAFKA_LISTENER_SECURITY_PROTOCOL_MAP="PLAINTEXT:PLAINTEXT"
 [ "${KAFKA_INTER_BROKER_LISTENER_NAME}" != "" ] || KAFKA_INTER_BROKER_LISTENER_NAME="PLAINTEXT"
-[ "${KAFKA_OPTS}" != "" ] || KAFKA_OPTS="-javaagent:/usr/jolokia/agents/jolokia-jvm.jar=host=0.0.0.0"
+# [ "${KAFKA_OPTS}" != "" ] || KAFKA_OPTS="-javaagent:/usr/jolokia/agents/jolokia-jvm.jar=host=0.0.0.0"
 [ "${KAFKA_TOPICS}" != "" ] || KAFKA_TOPICS="repos.created"
 
 function create_topics {
